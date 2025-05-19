abf295862c0f5267b7ce6e558a71a6c007c09281
Sayantan <sayantan.tapadar9@gmail.com>
2025-05-19 17:43:28 +0530

    dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,9 +1,41 @@
 
-
+function updateDashboardUI(data) {
+    // Update the dashboard with the received data
+    // Example: Update a div with the data
+    dashNetworking.innerHTML = data.networking;
+    dashInfos.innerHTML = data.infos;
+    dashInvis.innerHTML = data.invis;
+    dashPlans.innerHTML = data.plans;
+}
 
 
 //============================
 // AJAX methods
 //============================
 
+function getDashboardData() {
+    $.ajax({
+        url: '/dashboard/getData',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            week: getCurrWeek()
+        },
+        success: function(data) {
+            // Update the dashboard with the received data
+            // console.log('Dashboard data received:', data);
+            // Call the updateDashboard function to update the UI
+            updateDashboardUI(data);
+        },
+        error: function(xhr, status, error) {
+            console.error('Error fetching dashboard data:', error);
+        }
+    });
+}
 
+//============================
+// Loading methods
+//============================
+
+getDashboardData();
+dashCurrWeek.innerHTML = "Week " + getCurrWeek();
--- router/dashboard.js
+++ router/dashboard.js
@@ -1,19 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const dbm = require("../lib/dbmanager");
 
 const requireAuth = (req, res, next) => {
-    // if (req.session.userId) {
-    //     next(); // User is authenticated, continue to next middleware
-    // } else {
-    //     res.redirect('/login'); // User is not authenticated, redirect to login page
-    // }
-    next();
-}
+  // if (req.session.userId) {
+  //     next(); // User is authenticated, continue to next middleware
+  // } else {
+  //     res.redirect('/login'); // User is not authenticated, redirect to login page
+  // }
+  next();
+};
 
-router.get('/', requireAuth, async function (req, res) {
-    res.render('dashboard', { title: 'Dashboard' });
+router.get("/", requireAuth, async function (req, res) {
+  res.render("dashboard", { title: "Dashboard" });
 });
 
+router.post("/getData", requireAuth, async function (req, res) {
+  const week = req.body.week;
+  const d = new Date();
+  let year = d.getFullYear();
+  const data = await dbm.getDashboardData(week, year);
+//   console.log("Dashboard data:", data);
+  res.json(data);
+});
 
 module.exports = router;
